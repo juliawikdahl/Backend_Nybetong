624d80e31c49f82ec1879c331ca6b3d8c209fcdd
Julia Wikdahl <julia_2000@live.se>
2023-01-30 11:28:37 +0100

    AddedKontaktInformation

--- Backend_Nybetong/Backend_Nybetong/Models/MainModels/MainKontaktInformationModel.cs
+++ Backend_Nybetong/Backend_Nybetong/Models/MainModels/MainKontaktInformationModel.cs
@@ -2,5 +2,16 @@
 {
     public class MainKontaktInformationModel
     {
+        public int KIID { get; set; }
+        public int FID { get; set; }
+        public int KKID { get; set; }
+        public string Förnamn { get; set; }
+        public string Efternamn { get; set; }
+        public string Gatuadress { get; set; }
+        public string Postadress { get; set; }
+        public string Telefon { get; set; }
+        public string Mejl { get; set; }
+        public bool Aktiv { get; set; }
+
     }
 }
