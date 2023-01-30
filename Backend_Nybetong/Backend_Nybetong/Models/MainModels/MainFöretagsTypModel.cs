5e9e6c6f59d3559bbf3d41d098f8ef74f6ee01cf
Mary Wikdahl <mary.wikdahl@utb.ecutbildning.se>
2023-01-30 11:29:14 +0100

    AddedMAinModels

---
+++ Backend_Nybetong/Backend_Nybetong/Models/MainModels/MainFöretagsTypModel.cs
@@ -0,0 +1,9 @@
+namespace Backend_Nybetong.Models.MainModels
+{
+    public class MainFöretagsTypModel
+    {
+        public int FBID { get; set; }
+        public string Typ { get; set; }
+       
+    }
+}
---
+++ Backend_Nybetong/Backend_Nybetong/Models/MainModels/MainKontaktKategoriModel.cs
@@ -0,0 +1,9 @@
+namespace Backend_Nybetong.Models.MainModels
+{
+    public class MainKontaktKategori
+    {
+        public int KKID { get; set; }
+        public string Roll {get; set;}
+        
+    }
+}
